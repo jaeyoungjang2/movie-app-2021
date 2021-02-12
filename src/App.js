800c4f0ed72e8fb8f570e745bf618707d953d618
jaeyoungjang2 <lion85488@gmail.com>
2021-02-12 01:35:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+const foodILike = [{
+  id: 1,
+  name: "Kimchi",
+  image: "https://cdn.pixabay.com/photo/2019/07/25/01/35/kimchi-4361465_1280.jpg"
+},
+{
+  id: 2,
+  name: "Samgyeopsal",
+  image: "https://cdn.pixabay.com/photo/2015/11/20/08/17/meat-1052571_1280.jpg"
+},
+{
+  id: 3,
+  name: "Bibimbap",
+  image: "https://cdn.pixabay.com/photo/2017/08/08/09/44/food-photography-2610863_1280.jpg"
+}
+];
+
+// function renderFood(dish){
+//   return <Food name={dish.name} picture={dish.image}/>
+// }
+
+function Food({name, picture}){
+  return(
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture}/>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* dish(red color) is object */}
+      {/* key is just for react, using unique key, for remove warning message */}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image}/>)}
+      {/* {foodILike.map(renderFood)} */}
     </div>
   );
 }
