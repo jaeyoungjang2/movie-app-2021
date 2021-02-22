000c140ade7388a39fe628c364b5aadffad0ea81
jaeyoungjang2 <lion85488@gmail.com>
2021-02-22 21:34:14 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
-
-
-const foodILike = [{
-  id: 1,
-  name: "Kimchi",
-  image: "https://cdn.pixabay.com/photo/2019/07/25/01/35/kimchi-4361465_1280.jpg"
-},
-{
-  id: 2,
-  name: "Samgyeopsal",
-  image: "https://cdn.pixabay.com/photo/2015/11/20/08/17/meat-1052571_1280.jpg"
-},
-{
-  id: 3,
-  name: "Bibimbap",
-  image: "https://cdn.pixabay.com/photo/2017/08/08/09/44/food-photography-2610863_1280.jpg"
-}
-];
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./Movie.css";
 
 // function renderFood(dish){
 //   return <Food name={dish.name} picture={dish.image}/>
 // }
 
-function Food({name, picture}){
-  return(
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture}/>
-    </div>
-  )
+
+
+class App extends React.Component{
+  state ={
+    isLoading: true,
+    movies: []
+  };
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({isLoading: false});      
+  //   }, 6000)
+  // }
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json.sort_by=rating")
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+    <section className="container">
+      {isLoading 
+      ? (<div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>) 
+      : (
+        <div className="movies">
+          {movies.map(movie => {
+      console.log(movie);
+      return <Movie
+      key={movie.id} 
+      id={movie.id} 
+      year={movie.year} 
+      title={movie.title} 
+      summary={movie.summary} 
+      poster={movie.medium_cover_image}
+      genres={movie.genres} />
+    })}
+        </div>
+      )}
+      </section>
+  )}
+
 }
 
-function App() {
-  return (
-    <div className="App">
-      {/* dish(red color) is object */}
-      {/* key is just for react, using unique key, for remove warning message */}
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image}/>)}
-      {/* {foodILike.map(renderFood)} */}
-    </div>
-  );
-}
+
 
 export default App;
