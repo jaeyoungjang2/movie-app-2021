55a575863e5f49fef603479353a4ea8e1efda69a
jaeyoungjang2 <lion85488@gmail.com>
2021-02-24 23:24:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import Movie from "./Movie";
-import "./Movie.css";
+import { HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
-// function renderFood(dish){
-//   return <Food name={dish.name} picture={dish.image}/>
-// }
-
-
-
-class App extends React.Component{
-  state ={
-    isLoading: true,
-    movies: []
-  };
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({isLoading: false});      
-  //   }, 6000)
-  // }
-  getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json.sort_by=rating")
-    this.setState({ movies, isLoading: false });
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-    <section className="container">
-      {isLoading 
-      ? (<div className="loader">
-        <span className="loader__text">Loading...</span>
-      </div>) 
-      : (
-        <div className="movies">
-          {movies.map(movie => {
-      console.log(movie);
-      return <Movie
-      key={movie.id} 
-      id={movie.id} 
-      year={movie.year} 
-      title={movie.title} 
-      summary={movie.summary} 
-      poster={movie.medium_cover_image}
-      genres={movie.genres} />
-    })}
-        </div>
-      )}
-      </section>
-  )}
-
+function App(){
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={Detail} />
+  </HashRouter>
 }
-
-
 
 export default App;
